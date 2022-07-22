ac4a1ef455615b998052847e89cefbb6eef3fb45
Anderson Almada <andersonalmadagomes@gmail.com>
2022-07-22 16:42:27 -0300

    first commit

---
+++ src/main/java/br/ufc/mandacaru5/Mandacaru5Application.java
@@ -0,0 +1,13 @@
+package br.ufc.mandacaru5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mandacaru5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mandacaru5Application.class, args);
+	}
+
+}
